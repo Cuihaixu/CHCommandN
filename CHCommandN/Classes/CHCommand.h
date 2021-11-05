05d4b051f93db9dad3a1fa5b36207d12f2372422
cu <cu@cudeMacBook-Pro.local>
2021-11-05 14:16:00 +0800

    增加头文件

---
+++ CHCommandN/Classes/CHCommand.h
@@ -0,0 +1,18 @@
+//
+//  CHCommand.h
+//  CHCommandN
+//
+//  Created by cu on 2021/11/5.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CHCommand : NSObject
+
++ (void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CHCommandN/Classes/CHCommandN.h
@@ -0,0 +1,20 @@
+//
+//  CHCommandN.h
+//  CHCommandN
+//
+//  Created by cu on 2021/11/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CHCommandN.
+FOUNDATION_EXPORT double CHCommandNVersionNumber;
+
+//! Project version string for CHCommandN.
+FOUNDATION_EXPORT const unsigned char CHCommandNVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CHCommandN/PublicHeader.h>
+
+#import <CHCommandN/CHCommand.h>
+
+
